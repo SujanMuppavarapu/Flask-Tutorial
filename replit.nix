8a82a85a70511040dc08cf3daaf306afacf7fea7
SujanMuppavarapu <145279544+SujanMuppavarapu@users.noreply.github.com>
2023-09-17 16:20:40 +0000

    Added app.py and reseted replit.nix

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+        pkgs.python39Packages.flask
+        pkgs.python39Packages.flask
+        pkgs.python39Packages.pip
+        pkgs.bashInteractive
+        pkgs.python39Full
+    ];
+}
